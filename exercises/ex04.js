9beb33b4b3ba944e14f31cc59ad376552513709e
njmola-ops <gjmola@ucsc.edu>
2026-04-23 13:02:04 -0700

    n

--- exercises/ex04.js
+++ exercises/ex04.js
@@ -1,18 +1,21 @@
 let count=0;
 let colors=["Orchid", "Coral", "HotPink", "Plum"];
+let colorCount;
 
 $("#needy-button").click( function(){
-    
-    
 
-    $("#needy-button").html("You clicked me " + count + " times and your favourite color today is: " + colors[count] );
+    count = count +1; 
 
-    $("#needy-button").css( "background-color", colors[count]);  
+    colorCount=count-1;
 
-    count = count +1;
+    $("#needy-button").html("you clicked me " + count + " and your favourite color today is: " + colors[colorCount] );
 
-    $("html").css( "background-color", colors[count]);
+    $("#needy-button").css( "background-color", colors[colorCount]);  
 
-    
+    $("html").css( "background-color", colors[colorCount+1]);
+
+// things to try out:
+// make the page background color two colors ahead of the button color
+// make the page background color one color beyond of the button color
 
 });
