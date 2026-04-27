ad7bef0e5e9182795b1d59052624d3faa49dff5c
njmola-ops <gjmola@ucsc.edu>
2026-04-26 18:30:24 -0700

    hhhh

--- environ/env03.js
+++ environ/env03.js
@@ -1,6 +1,6 @@
 let environmentTitle = "Peaceful Galaxy";
 
-let environmentElements = ["aliens", "stars", "planets", "sun"];
+let environmentElements = ["aliens", "astroids", "planets", "sun"];
 
 let mainEntity = {
   name: "aliens",
@@ -8,24 +8,23 @@ let mainEntity = {
   mood: ["faint","strong","unstable","clear","blinking","abnormal","changing","dangerous","strange"],
   isMoving: true
 };
-let count = 0;
+
+let lifeCount = 0;
 
 $("#lifebutton").click(function () {
+  lifeCount++;
+  $(this).html(`Scanned for life ${lifeCount} times`);
 
-  count++;
-
-  $(this).html(`Scanned for life ${count} times`);
-
-  let lifeforms = count + 2;
-
-  let index = count % mainEntity.mood.length;
+  let lifeforms = lifeCount + 2;
+  let index = lifeCount % mainEntity.mood.length;
   let currentMood = mainEntity.mood[index];
 
- $("#output").html(`
-  <p class="scan-text">${lifeforms} lifeforms have been identified.</p>
-  <p class="scan-text">The signal is ${currentMood}.</p>
-`);
+  $("#output").html(`
+    <p class="scan-text1">${lifeforms} lifeforms have been identified.</p>
+    <p class="scan-text1">The signal is ${currentMood}.</p>
+  `);
 });
+
 $("#playBtn").click(function () {
   let audio = $("#sound")[0];
 
@@ -37,30 +36,25 @@ $("#playBtn").click(function () {
     $(this).text("Play Sound");
   }
 });
-  if (audio.paused) {
-    audio.play();
-    $(this).text("Pause Sound");
-  } else {
-    audio.pause();
-    $(this).text("Play Sound");
-  }
-  
-let sunFacts = [
-  "The Sun is about 93 million miles from Earth.",
-  "The Sun made mostly of hydrogen and helium.",
-  "The Sun accounts for 99.8% of the solar system’s mass.",
-  "Light from the Sun takes about 8 minutes to reach Earth.",
-  "The core of the Sun is about 15 million °C.",
-  "The Sun produces energy through nuclear fusion."
-];
+let duoEntity = {
+  name: "astroids",
+  type: "extraterrestrial rock",
+  mood: ["low","moderate","high","moderate"],
+  isMoving: true
+};
 
-$("#sunBtn").click(function () {
+let rockCount = 0;
 
-  let randomIndex = Math.floor(Math.random() * sunFacts.length);
-  let fact = sunFacts[randomIndex];
+$("#rockbutton").click(function () {
+  rockCount++;
+  $(this).html(`Scanned for astroids ${rockCount} times`);
+
+  let astroids = rockCount + 3;
+  let index = rockCount % duoEntity.mood.length;
+  let currentMood = duoEntity.mood[index];
 
   $("#output").html(`
-    <p class="scan-text">${fact}</p>
+    <p class="scan-text2">${astroids} astroids have been identified.</p>
+    <p class="scan-text2">Collision risk is ${currentMood}.</p>
   `);
-
 });
