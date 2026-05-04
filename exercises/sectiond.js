32e52626647367cc96ec0c3a2fa2badaffafa5ee
njmola-ops <gjmola@ucsc.edu>
2026-05-04 14:53:22 -0700

    xo

---
+++ exercises/sectiond.js
@@ -0,0 +1,12 @@
+let person1 = "Miranda" ;
+let person2 = "Angel" ;
+let money1 = 5 ;
+let money2 = 5 ;
+
+function coffee_purchase(person1, money1)
+{
+if (money1 > 5 && money1 == 5);
+    {
+     print (perso1 + "does not get coffee")
+    }
+}
